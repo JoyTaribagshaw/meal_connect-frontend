3a96edac583169e9ab5b8faeac13293f13164322
BranBayou <berhan.baye@gmail.com>
2023-12-02 17:40:56 +0300

    Update app component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import backgroundImage from '../img/meal1.jpg';
+import backgroundImage from '../img/meals/meal77.jpg';
 import logo from '../img/logo2.png';
 
 const Login = () => (
@@ -17,20 +17,26 @@ const Login = () => (
 
     <div className="flex-1 flex flex-col justify-center items-center">
 
-      <span className="w-16 mb-5 border-b-2 border-orange-500" />
-      <h3 className="mb-4 text-center ">Welcome back !</h3>
+      <span className="w-16 mb-5 border-b-2 border-orange" />
+      <h3 className="mb-4 text-center font-bold">Welcome back !</h3>
 
       <div className="flex justify-center items-center">
         <div className="w-full max-w-xs">
           <form className="bg-white px-8 pt-6 pb-8 mb-4">
             <div className="mb-4">
-              <input type="email" id="email" name="email" required className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+              <input type="email" id="user-email" name="email" required className="shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Email" />
             </div>
             <div className="mb-6">
-              <input type="password" id="password" name="password" required className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+              <input type="password" id="user-password" name="password" required className="shadow appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Password" />
             </div>
             <div className="flex items-center justify-between">
-              <input type="submit" value="Login" className="bg-green hover:bg-orange text-white font-bold py-2 px-4 rounded-2xl focus:outline-none focus:shadow-outline cursor-pointer w-full" />
+              <input type="submit" value="Login" className="bg-green hover:bg-orange text-white font-bold py-2 px-4 rounded-xl focus:outline-none focus:shadow-outline cursor-pointer w-full" />
+            </div>
+            <div className="text-center py-6">
+              <a href="#Signup" className="text-red-500 self-center hover:text-green">Sign-up</a>
+            </div>
+            <div className="text-center">
+              <h4>If you dont have an accout.</h4>
             </div>
           </form>
         </div>
