9c949282f2d6934eaf84d159db6904f8f63e8764
BranBayou <berhan.baye@gmail.com>
2023-12-04 19:48:20 +0300

    Add detail-page for meal

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import Splash from './components/Splash';
 import Login from './components/Login';
 import Signup from './components/SignUp';
 import Main from './components/Main';
-<<<<<<< Updated upstream
 import ReservationPage from './components/ReservationPage';
-=======
 import Details from './components/Details';
->>>>>>> Stashed changes
 
 function App() {
   return (
@@ -15,11 +12,8 @@ function App() {
       <Login />
       <Signup />
       <Main />
-<<<<<<< Updated upstream
-      <ReservationPage />
-=======
       <Details />
->>>>>>> Stashed changes
+      <ReservationPage />
     </div>
   );
 }
