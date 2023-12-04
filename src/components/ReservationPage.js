fc913befb7583243de9e284a1260d7bacbc094c7
GhostEsso <lilipitaham@gmail.com>
2023-12-04 11:02:57 +0000

    list of lateral bar added

--- src/components/ReservationPage.js
+++ src/components/ReservationPage.js
@@ -15,3 +15,18 @@ const ReservationPage = () => {
         image: meal1,
       },
     ];
+
+    return (
+        <div className="h-screen flex justify-between">
+          <div className="flex flex-col justify-between border-r-4 border-gray-100 p-0 w-1/4 ml-2 mb-2">
+            <div className="">
+              <img src={logo} alt="logoImg" className="w-36 mb-3" />
+              <ul className="flex flex-col gap-0 ">
+                <li className="w-full bg-green hover:bg-green active:bg-green focus:ring hover:text-white p-2"><a href="#new" className="">Meal</a></li>
+                <li className="w-full hover:bg-green active:bg-green hover:text-white p-2"><a href="#new" className="">Reserve</a></li>
+                <li className="w-full hover:bg-green active:bg-green hover:text-white p-2"><a href="#new" className="">My Reservation</a></li>
+                <li className="w-full hover:bg-green active:bg-green hover:text-white p-2"><a href="#new" className="">Add meal</a></li>
+                <li className="w-full hover:bg-green active:bg-green hover:text-white p-2"><a href="#new" className="">Delete meal</a></li>
+              </ul>
+            </div>
+        
