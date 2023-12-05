bdcc56735460ce6cb5c8607845837dd6816ebade
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-12-05 12:40:12 +0000

    update styling for delete meal UI

--- src/components/DeleteMeal.js
+++ src/components/DeleteMeal.js
@@ -6,8 +6,8 @@ function DeleteMeal() {
     <div className="mx-auto my-8 p-6 bg-white rounded-md">
       <h1 className="text-2xl font-bold mb-10">My Meal(s):</h1>
       <div className="flex flex-col">
-        <div className="bg-white rounded-lg shadow-md border-gray-300 border p-7 mr-1 mb-4 flex items-center justify-between">
-          <img src={meal1} alt="meal-img" className="w-16 h-16 object-cover rounded mr-4" />
+        <div className="bg-white rounded-lg shadow-md border-gray-300 border p-7 mr-4 mb-4 flex items-center justify-between">
+          <img src={meal1} alt="meal-img" className="w-25 h-20 object-cover rounded mr-4" />
           <h2 className="text-lg font-semibold flex-grow text-center">Title</h2>
           <button type="button" className="bg-red-500 text-white py-2 px-4 rounded">Delete</button>
         </div>
