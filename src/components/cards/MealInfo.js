355de563800800c00a1fa32e23f271812d5c95f3
GhostEsso <lilipitaham@gmail.com>
2023-12-07 19:02:54 +0000

    Header added

--- src/components/cards/MealInfo.js
+++ src/components/cards/MealInfo.js
@@ -59,9 +59,23 @@ function MealInfo() {
   };
 
   return (
-    <div className="w-4/5 h-4/5 my-auto meal-detail-container flex">
-      <div className="w-2/5">
+    <div className="w-4/5">
+      <div className="w-full mb-32 text-center mt-10">
+        <h2 className="text-2xl font-bold">Reserve Your Meal 🥙 🍲 🥗</h2>
         {mealDetails && (
+          <div className="meal-info flex p-4 gap-10 justify-center items-center mt-2 text-xs">
+            <p>{mealDetails.name}</p>
+            <p>
+              <span className="text-gray-500">Price💸:</span>
+              {` ${mealDetails.price} $`}
+            </p>
+          </div>
+        )}
+      </div>
+
+      <div className="w-4/5 h-4/5 my-auto meal-detail-container flex justify-between">
+        <div className="w-2/5">
+          {mealDetails && (
           <div
             key={mealDetails.id}
             className="meal-card flex items-center justify-center text-center"
@@ -74,70 +88,72 @@ function MealInfo() {
               />
             </div>
           </div>
-        )}
-      </div>
-      <div className=" w-2/5">
-        <form onSubmit={handleSubmit}>
-          <div className="mb-6 w-full">
-            <input
-              type="number"
-              name="quantity"
-              value={state.quantity}
-              onChange={handleChange}
-              id="quantity"
-              step="any"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder=" Quantity"
-              required
-            />
-          </div>
+          )}
+        </div>
+        <div className=" w-2/5">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-6 w-full">
+              <input
+                type="number"
+                name="quantity"
+                value={state.quantity}
+                onChange={handleChange}
+                id="quantity"
+                step="any"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder=" Quantity"
+                required
+              />
+            </div>
 
-          <div className="mb-6 w-full">
-            <select
-              name="spicy_level"
-              value={state.spicy_level}
-              onChange={handleChange}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required
-            >
-              <option value="" disabled>
-                Choose spicy Level
-              </option>
-              <option value="Normal">Normal</option>
-              <option value="Hot">Hot</option>
-              <option value="Cold">Cold</option>
-            </select>
+            <div className="mb-6 w-full">
+              <select
+                name="spicy_level"
+                value={state.spicy_level}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+              >
+                <option value="" disabled>
+                  Choose spicy Level
+                </option>
+                <option value="Normal">Normal</option>
+                <option value="Hot">Hot</option>
+                <option value="Cold">Cold</option>
+              </select>
 
-          </div>
-          <div className="mb-6 w-full">
-            <input
-              type="date"
-              name="reserve_date"
-              value={state.reserve_date}
-              onChange={handleChange}
-              id="reserve_date"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder=" Reserve date"
-              required
-            />
-          </div>
-          <div className="mb-6 w-full">
-            <input
-              type="time"
-              name="reserve_time"
-              value={state.reserve_time}
-              onChange={handleChange}
-              id="reserve_date"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder=" Reserve time"
-              required
-            />
-          </div>
-          <button type="submit" className="text-white bg-[#72b600] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Reserve</button>
-        </form>
+            </div>
+            <div className="mb-6 w-full">
+              <input
+                type="date"
+                name="reserve_date"
+                value={state.reserve_date}
+                onChange={handleChange}
+                id="reserve_date"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder=" Reserve date"
+                required
+              />
+            </div>
+            <div className="mb-6 w-full">
+              <input
+                type="time"
+                name="reserve_time"
+                value={state.reserve_time}
+                onChange={handleChange}
+                id="reserve_date"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder=" Reserve time"
+                required
+              />
+            </div>
+            <button type="submit" className="text-white bg-[#72b600] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Reserve</button>
+          </form>
+        </div>
+        <Link to="/dashboard" className="flex justify-start text-xl bg-[#72b600] w-12 p-2 z-50 absolute text-center left-0 bottom-[50%] text-white rounded-r-full">&#x25c0;</Link>
       </div>
-      <Link to="/dashboard" className="flex justify-start text-xl bg-[#72b600] w-12 p-2 z-50 absolute text-center left-0 bottom-0 text-white rounded-r-full">&#x25c0;</Link>
     </div>
+
   );
 }
 
