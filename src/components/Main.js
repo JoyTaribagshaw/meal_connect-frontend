fce1abdfe2777a29ef9f8f57f1eed3acfbf7dca3
BranBayou <berhan.baye@gmail.com>
2023-12-07 21:21:52 +0300

    Add responsiveness to main screen

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,7 +14,7 @@ function Main() {
   }, [navigate, user]);
 
   return (
-    <div className="flex">
+    <div className="flex flex-col vss:flex-row">
       <Navigation />
       <MealSlider />
     </div>
