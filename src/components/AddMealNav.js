398fc6a259394658d67179a6fe9d5637db13ddea
BranBayou <berhan.baye@gmail.com>
2023-12-08 15:53:48 +0300

    Add MealInfo responsive

--- src/components/AddMealNav.js
+++ src/components/AddMealNav.js
@@ -2,7 +2,7 @@ import AddMeal from './AddMeal';
 import Navigation from './navigation/Navigation';
 
 const AddMealNav = () => (
-  <div className="flex">
+  <div className="flex flex-col ss:flex-row">
     <Navigation />
     <AddMeal />
   </div>
