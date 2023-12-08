208bb77092c5c0aeffb2a0e9f6463aa20ef22b18
BranBayou <berhan.baye@gmail.com>
2023-12-08 14:49:53 +0300

    Add mobile version for navigation

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
+import { VscMenu } from 'react-icons/vsc';
+import { IoMdClose } from 'react-icons/io';
 import { userLoggedOut } from '../../features/login/loginSlice';
+import './navigation.css';
 import logo from '../../img/logo1.png';
 import tw from '../../img/tw.svg';
 import fb from '../../img/fb.svg';
@@ -19,16 +22,22 @@ function Navigation() {
     navigate('/login');
   };
   const isAdmin = user && user.data && user.data.admin;
+  const [isMobile, setIsMobile] = useState(false);
   return (
-    <div className="h-screen flex flex-col justify-between border-r-4 border-gray-100 p-0 w-[25%] ml-2 mb-2">
-      <div className="">
-        <img src={logo} alt="logoImg" className="w-36 mb-3" />
-        <ul className="flex flex-col gap-0 ">
-          <li className="w-full bg-green hover:bg-green active:bg-green focus:ring hover:text-white p-2"><Link to="/dashboard" className="">Meal</Link></li>
-          <li className="w-full hover:bg-green active:bg-green hover:text-white p-2"><Link to="/reservation" className="">Reserve</Link></li>
-          <li className="w-full hover:bg-green active:bg-green hover:text-white p-2"><Link to="/myReservations" className="">My Reservations</Link></li>
-          {isAdmin && (<li className="w-full hover:bg-green active:bg-green hover:text-white p-2"><Link to="/addmeal" className="">Add meal</Link></li>)}
-          <li className="w-full hover:bg-green active:bg-green hover:text-white p-2"><Link to="/deleteMeal" className="">Delete meal</Link></li>
+    <div className="nav h-screen flex flex-col :flex-row justify-between border-r-4 border-gray-100 p-0 w-[25%] ml-2 mb-2">
+      <div className="header">
+        <div className="ss:w-full flex flex-row ss:flex-col justify-between items-center">
+          <img src={logo} alt="logoImg" className="w-20 mb-3 ss:w-36" />
+          <button className="mx-3 text-orange w-9" type="button" onClick={() => setIsMobile(!isMobile)}>
+            {isMobile ? (<span className="block ss:hidden"><VscMenu /></span>) : (<span className="block ss:hidden"><IoMdClose /></span>) }
+          </button>
+        </div>
+        <ul className={`flex flex-col gap-0 ss:block ${isMobile ? 'hidden' : 'top-20'}`}>
+          <li className="w-full bg-green hover:bg-green active:bg-green focus:ring hover:text-white p-2 border-b-2 sm:border-none sm:border-b sm:hover:border-b-2"><Link to="/dashboard" className="">Meal</Link></li>
+          <li className="w-full hover:bg-green active:bg-green hover:text-white p-2 border-b-2 sm:border-none sm:border-b sm:hover:border-b-2"><Link to="/reservation" className="">Reserve</Link></li>
+          <li className="w-full hover:bg-green active:bg-green hover:text-white p-2 border-b-2 sm:border-none sm:border-b sm:hover:border-b-2"><Link to="/myReservations" className="">My Reservations</Link></li>
+          {isAdmin && (<li className="w-full hover:bg-green active:bg-green hover:text-white p-2 border-b-2 sm:border-none sm:border-b sm:hover:border-b-2"><Link to="/addmeal" className="">Add meal</Link></li>)}
+          <li className="w-full hover:bg-green active:bg-green hover:text-white p-2 border-b-2 sm:border-none sm:border-b sm:hover:border-b-2"><Link to="/deleteMeal" className="">Delete meal</Link></li>
           <li className="w-full hover:bg-green active:bg-green hover:text-white p-2">
             <button type="button" onClick={handleLogout} className="cursor-pointer">
               Logout
@@ -36,7 +45,7 @@ function Navigation() {
           </li>
         </ul>
       </div>
-      <div className="vss:w-full hidden vss:block">
+      <div className="ss:w-full hidden ss:block">
         <ul className="flex gap-3 p-2">
           <li><a href="#new"><img src={tw} alt="tw" className="w-4" /></a></li>
           <li><a href="#new"><img src={fb} alt="fb" className="w-4" /></a></li>
