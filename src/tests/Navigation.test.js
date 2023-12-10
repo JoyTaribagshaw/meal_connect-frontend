cb95f96cd1820d4d58f994b9e3542d62f9635f34
GhostEsso <lilipitaham@gmail.com>
2023-12-10 15:36:09 +0000

    test thats checks specific navigation links within the 'Navigation' component and ensures they are correctly rendered based on their names

--- src/tests/Navigation.test.js
+++ src/tests/Navigation.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import { Provider } from 'react-redux'; // Import du Provider de react-redux
-import store from '../app/store'; // Importez votre store Redux
+import { Provider } from 'react-redux';
+import store from '../app/store';
+import Navigation from '../components/navigation/Navigation';
 
-import Navigation from '../components/navigation/Navigation'; // Assurez-vous que le chemin d'accès au composant est correct
-
-test('renders Navigation component', () => {
+test('renders navigation links in Navigation component', () => {
   render(
     <Provider store={store}>
       <MemoryRouter>
@@ -14,5 +13,17 @@ test('renders Navigation component', () => {
       </MemoryRouter>
     </Provider>
   );
-});
 
+  // Check if specific navigation links are present
+  const mealLink = screen.getByRole('link', { name: /meal/i });
+  const reserveLink = screen.getByRole('link', { name: /reserve/i });
+  const myReservationsLink = screen.getByRole('link', { name: /my reservations/i });
+  const addMealLink = screen.queryByRole('link', { name: /add meal/i });
+  const deleteMealLink = screen.queryByRole('link', { name: /delete meal/i });
+
+  expect(mealLink).toBeInTheDocument();
+  expect(reserveLink).toBeInTheDocument();
+  expect(myReservationsLink).toBeInTheDocument();
+  expect(addMealLink).toBeNull(); // Assuming it's not present for non-admin users
+  expect(deleteMealLink).toBeNull(); // Assuming it's not present for non-admin users
+});
