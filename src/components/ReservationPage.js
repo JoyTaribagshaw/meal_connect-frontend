c27fd5f08a356468da15ad8b7cd4b26ed9f8b8aa
MargaretTechworld <margarettechworld@gmail.com>
2023-12-12 11:11:46 +0000

    Fixed reservation page

--- src/components/ReservationPage.js
+++ src/components/ReservationPage.js
@@ -12,23 +12,25 @@ const ReservationPage = () => {
   }, [dispatch]);
 
   const handleDelete = (id) => {
-    dispatch(deleteReservation(id)).then(() => {
-      dispatch(removeReservation(id));
-    }).catch((error) => {
-      throw error;
-    });
+    dispatch(deleteReservation(id))
+      .then(() => {
+        dispatch(removeReservation(id));
+      })
+      .catch((error) => {
+        throw error;
+      });
   };
 
   return (
     <div className="h-screen flex flex-col ss:flex-row">
       <Navigation />
-      <div className="container ss:overflow-scroll ss:mx-4 mt-8">
+      <div className="container ss:overflow-scroll mx-auto mt-8 p-10">
         <h1 className="text-2xl font-bold mb-10 ml-8">My Reservation(s):</h1>
-        <div className="flex flex-col">
+        <div className="flex flex-col items-center">
           {reservationList.map((reservation) => (
             <div
               key={reservation.reservation.id}
-              className="bg-white rounded-lg shadow-md border-gray-300 border p-5 mr-1 mb-4 flex flex-col ss:flex-row items-center justify-between"
+              className="bg-white rounded-lg shadow-md border-gray-300 border p-5 mb-4 flex flex-col ss:flex-row items-center justify-between w-full ss:w-full"
             >
               <img
                 src={reservation.meal.photo}
@@ -39,32 +41,22 @@ const ReservationPage = () => {
                 <h2 className="text-lg font-bold flex-grow text-center">
                   {reservation.meal.name}
                 </h2>
-                <p className="flex-grow text-center" >
-                  Total price:
-                  {' '}
-                  {reservation.total.total_price}
-                  {' '}
-                  $
+                <p className="text-center">
+                  Total price: {reservation.total.total_price} $
                 </p>
-                <p className="flex-grow text-center">
-                  Quantity:
-                  {reservation.reservation.quantity}
+                <p className="text-center">
+                  Quantity: {reservation.reservation.quantity}
                 </p>
-                <p className="flex-grow text-center">
-                  Spicy:
-                  {reservation.reservation.spicy_level}
+                <p className="text-center">
+                  Spicy: {reservation.reservation.spicy_level}
                 </p>
               </div>
               <div>
-                <h2 className="flex-grow text-center">
-                  Date:
-                  {' '}
-                  {reservation.reservation.reserve_date}
+                <h2 className="text-center">
+                  Date: {reservation.reservation.reserve_date}
                 </h2>
-                <p className=" flex-grow text-center">
-                  Time:
-                  {' '}
-                  {reservation.reservation.reserve_time}
+                <p className="text-center">
+                  Time: {reservation.reservation.reserve_time}
                 </p>
               </div>
               <button
